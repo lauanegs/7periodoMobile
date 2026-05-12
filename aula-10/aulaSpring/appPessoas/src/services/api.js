a1a7545ea7ac4b9c2666bf11494b75ad201cde3b
Lauane Gonzaga e Silva <130411983+lauanegs@users.noreply.github.com>
2026-05-12 18:54:09 +0000

    atualizando

---
+++ aula-10/aulaSpring/appPessoas/src/services/api.js
@@ -0,0 +1,16 @@
+// src/services/api.js
+
+import axios from 'axios'
+
+// Troque pelo endereço público do seu Codespace com a porta 8080 exposta
+const BASE_URL = https://super-duper-meme-qw9rp94qx963xgqg-8081.app.github.dev/
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+export default api
